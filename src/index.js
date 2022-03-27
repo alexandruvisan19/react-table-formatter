f729fa2a98dafb59bd15624713e42cf88eac7aee
Alexandru Visan <alexandruvisan1908@gmail.com>
2022-03-27 11:52:59 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import SheetJSApp from "./SheetJSApp";
+import "./index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <SheetJSApp />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
